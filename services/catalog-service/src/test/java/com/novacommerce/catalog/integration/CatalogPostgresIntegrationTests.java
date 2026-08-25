caf3e4ecd0cc2194a7fee559a3fee5256f22a168
Niloy Datta <niloy.datta.dev@gmail.com>
2026-08-25 17:52:27 +0600

    test: update ddl-auto configuration for postgres integration tests

--- services/catalog-service/src/test/java/com/novacommerce/catalog/integration/CatalogPostgresIntegrationTests.java
+++ services/catalog-service/src/test/java/com/novacommerce/catalog/integration/CatalogPostgresIntegrationTests.java
@@ -35,6 +35,9 @@ class CatalogPostgresIntegrationTests {
         registry.add("spring.datasource.url", POSTGRES::getJdbcUrl);
         registry.add("spring.datasource.username", POSTGRES::getUsername);
         registry.add("spring.datasource.password", POSTGRES::getPassword);
+        registry.add("spring.flyway.url", POSTGRES::getJdbcUrl);
+        registry.add("spring.flyway.user", POSTGRES::getUsername);
+        registry.add("spring.flyway.password", POSTGRES::getPassword);
         registry.add("spring.jpa.hibernate.ddl-auto", () -> "validate");
         registry.add("spring.flyway.enabled", () -> "true");
         registry.add("spring.data.redis.host", () -> "127.0.0.1");
